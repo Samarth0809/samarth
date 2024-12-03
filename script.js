e05312ee308eab1c22ff5fe2850a14a54690cf1a
Samarthjagakar <56903195+Samarth0809@users.noreply.github.com>
2024-12-03 21:11:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Select the input toggle switch
+const themeToggle = document.querySelector('.switch .input');
+
+// Listen for toggle change
+themeToggle.addEventListener('change', () => {
+    // Toggle the dark-theme class on the body
+    document.body.classList.toggle('dark-theme');
+});
+
+
+// Display an alert message when the page is loaded
+window.onload = function() {
+    alert("Welcome to Samarth Murlidhar Jagakar's Portfolio!");
+};
